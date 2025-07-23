cb13c5484db279808f9b1fd33db03326063dc497
F5 <you@example.com>
2025-07-23 15:19:29 -0400

    Configuración para Cloud Run con Cloud SQL

--- app.py
+++ app.py
@@ -115,5 +115,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
